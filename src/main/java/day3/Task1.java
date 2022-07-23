b8c60655d20e3ce5f19a1b03062f9329e3d77f6d
Ivan Smirnov <nn52-ivan@outlook.com>
2022-07-23 07:52:22 -0400

    Day 3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,33 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true){
+            String city = scanner.nextLine();
+
+            if (city.equals("Stop"))
+                break;
+
+            switch (city){
+                case "Москва":
+                    System.out.println("Россия");
+                    break;
+                case "Милан" :
+                    System.out.println("Италия");
+                    break;
+                case "Лондон" :
+                    System.out.println("Англия");
+                    break;
+                case "Мюнхен" :
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
