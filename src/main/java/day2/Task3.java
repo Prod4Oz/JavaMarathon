b87fed9cc860f76857cbacc20cb9bbaeb14839e5
Ivan Smirnov <nn52-ivan@outlook.com>
2022-07-23 06:05:57 -0400

    Day 2

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int t = a + 1;
+        if (a < b && b != 0) {
+            while (t < b) {
+                if (t % 5 == 0 && t % 10 != 0) System.out.print(t + " ");
+                t++;
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
