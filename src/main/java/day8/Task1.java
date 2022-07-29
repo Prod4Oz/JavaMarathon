c9b30ff14a5df5a41c4f1cd8709f4827b3a300d2
Ivan Smirnov <nn52-ivan@outlook.com>
2022-07-29 13:24:09 -0400

    Day 9

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,28 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+    String s = "";
+
+    long before = System.currentTimeMillis();
+
+    for (int i = 0; i<20001; i++ ) s += i + " ";
+
+    long after = System.currentTimeMillis();
+        System.out.println("Time String -> "+(after-before));
+//        System.out.println(s);
+
+
+        StringBuilder sb = new StringBuilder("");
+
+        before = System.currentTimeMillis();
+        for (int i = 0; i<=20000; i++ )
+            sb.append(i).append(" ");
+        after = System.currentTimeMillis();
+//        System.out.println(sb);
+        System.out.println("Time StringBuilder -> "+ (after-before));
 
     }
+
+
+
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,35 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Figure [] figures = {
+                new Triangle(9,9,9,"Black"),
+                new Triangle(5,7,9,"Red"),
+                new Triangle(5,8,4,"Red"),
+                new Rectangle("Gray", 45,15),
+                new Rectangle("Red", 12,45),
+                new Circle(7,"White"),
+                new Circle(21,"Red")
+        };
+        System.out.println(calculateRedArea(figures));
+        System.out.println(calculateRedPerimeter(figures));
+    }
 
+    public static double calculateRedPerimeter ( Figure [] figures){
+        double sum = 0;
+
+        for (Figure figure : figures) {
+            if (figure.getColor().equals("Red"))
+                sum += figure.perimeter();
+        }
+        return  sum;
+    }
+    public static double calculateRedArea ( Figure [] figures){
+        double sum = 0;
+
+        for (Figure figure : figures) {
+            if (figure.getColor().equals("Red"))
+                sum += figure.area();
+        }
+        return  sum;
     }
 }
